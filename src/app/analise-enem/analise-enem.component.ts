f1022a7bea396339ad7bb58e1273a54a72505fd3
Maykon Oliveira <maykonoliveira850@gmail.com>
2018-06-21 16:36:41 -0300

    Up - bugado ainda

--- src/app/analise-enem/analise-enem.component.ts
+++ src/app/analise-enem/analise-enem.component.ts
@@ -1,6 +1,8 @@
 import { EducacaoDadosAPIService } from "./../service/educacao-dados-api.service";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 import * as d3 from "d3";
+import { ViewChild } from "@angular/core";
+import { BaseChartDirective } from 'ng2-charts/ng2-charts';
 
 @Component({
   selector: "app-analise-enem",
@@ -9,29 +11,37 @@ import * as d3 from "d3";
 })
 export class AnaliseEnemComponent implements OnInit {
 
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective;
+
   dados: any[] = [{ data: [20], label: 'RN' }];
   public tipoGrafico: string = "bar";
 
   constructor(private api: EducacaoDadosAPIService) { }
 
   ngOnInit() {
-    let estados = ['RN', 'SP'];
+    let estados = ['RN'];
     estados.forEach(estado => {
       this.api.getEscolasByEstado(estado).subscribe(
         escolas => {
           let nota = [];
           escolas[1].forEach(escola => {
-            nota.push(escola.enemMediaGeral);
+            if (escola.enemMediaGeral > 0) {
+              nota.push(escola.enemMediaGeral);
+            }
           });
 
           this.addObjetToList(estado, d3.mean(nota));
         }
       );
     });
-
   }
 
-  addObjetToList(estado: string, nota: number) {
-    this.dados.push(`{"data": ${nota}, "label": ${estado}}`);
+  private addObjetToList(estado: string, nota: number) {
+    nota = Math.trunc(nota);
+
+    this.dados.push({data: [nota], label: estado});
+
+    // this.chart.chart.reset();
+    console.log(this.chart.datasets[0].data);
   }
 }
